6a91c0f27876a4f2214b7c83efaba568a4f7c590
vpro3611 <vprov8645@gmail.com>
2026-05-23 20:53:59 +0200

    Improve properties window attribute UX

--- src/FileSystemP.WPF/Views/PropertiesWindow.xaml.cs
+++ src/FileSystemP.WPF/Views/PropertiesWindow.xaml.cs
@@ -1,28 +1,86 @@
 using System.Windows;
+using System.Windows.Input;
 using FileSystemP.WPF.ViewModels;
 
 namespace FileSystemP.WPF.Views;
 
 public partial class PropertiesWindow : Window
 {
-    private PropertiesWindow(object metadata)
+    private readonly PropertiesViewModel _viewModel;
+
+    private PropertiesWindow(object metadata, Action? onChangesApplied)
     {
         InitializeComponent();
-        DataContext = new PropertiesViewModel(metadata);
+        _viewModel = new PropertiesViewModel(metadata, onChangesApplied: onChangesApplied);
+        DataContext = _viewModel;
     }
 
-    private void Close_Click(object sender, RoutedEventArgs e)
-    {
-        DialogResult = true;
-    }
+    public bool HasSavedChanges { get; private set; }
 
-    public static void ShowFor(object metadata, Window? owner)
+    public static bool ShowFor(object metadata, Window? owner, Action? onChangesApplied = null)
     {
-        var dialog = new PropertiesWindow(metadata)
+        var dialog = new PropertiesWindow(metadata, onChangesApplied)
         {
             Owner = owner
         };
 
         dialog.ShowDialog();
+        return dialog.HasSavedChanges;
+    }
+
+    private void Advanced_Click(object sender, RoutedEventArgs e)
+    {
+        AdvancedAttributesWindow.ShowFor(_viewModel, this);
+    }
+
+    private async void Apply_Click(object sender, RoutedEventArgs e)
+    {
+        await SaveChangesAsync(closeAfterSave: false);
+    }
+
+    private async void Ok_Click(object sender, RoutedEventArgs e)
+    {
+        if (!_viewModel.HasChanges)
+        {
+            DialogResult = true;
+            return;
+        }
+
+        await SaveChangesAsync(closeAfterSave: true);
+    }
+
+    private void Cancel_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+    }
+
+    private async Task SaveChangesAsync(bool closeAfterSave)
+    {
+        try
+        {
+            IsEnabled = false;
+            Mouse.OverrideCursor = Cursors.Wait;
+
+            await _viewModel.SaveChangesAsync();
+            HasSavedChanges = true;
+
+            if (closeAfterSave)
+            {
+                DialogResult = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(
+                ex.Message,
+                "Error applying attributes",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+        finally
+        {
+            Mouse.OverrideCursor = null;
+            IsEnabled = true;
+        }
     }
 }
