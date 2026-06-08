880150975982b6e1e528e2825ccb20cea905d473
vpro3611 <vprov8645@gmail.com>
2026-06-08 18:59:37 +0200

    feat: add CdResult record

--- src/FileSystemP.Core/CommandService/CommandPayloads.cs
+++ src/FileSystemP.Core/CommandService/CommandPayloads.cs
@@ -3,3 +3,5 @@ namespace FileSystemP.Core.CommandService;
 public sealed record FileContentResult(byte[] Content);
 
 public sealed record FileLinesResult(IReadOnlyList<string> Lines);
+
+public sealed record CdResult(IReadOnlyList<System.IO.FileSystemInfo> Entries, string FullPath);
