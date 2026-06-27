e055aba6eec20b9444b553bb0ec6933c90f9780c
vpro3611 <vprov8645@gmail.com>
2026-06-27 23:38:40 +0200

    add (backend): added new return type for backend to send signal to frontend to open a file

--- src/FileSystemP.Core/CommandService/CommandResult.cs
+++ src/FileSystemP.Core/CommandService/CommandResult.cs
@@ -1,7 +1,7 @@
 namespace FileSystemP.Core.CommandService;
 
 public sealed record CommandResult(bool Success, object? Payload = null, string? Message = null, bool ShouldExit = false, bool ShouldOpenProperties = false,
-    bool ShouldGoBack = false, bool ShouldGoForward = false, bool ShouldUndo = false, bool ShouldOpenSearch = false, bool ShouldToggleHidden = false, string? GoHomePath = null)
+    bool ShouldGoBack = false, bool ShouldGoForward = false, bool ShouldUndo = false, bool ShouldOpenSearch = false, bool ShouldToggleHidden = false, string? GoHomePath = null, bool ShouldOpenFile = false, string? PathToOpen = null)
 {
     public static CommandResult Ok(object? payload = null, string? message = null) => new(true, payload, message);
 
@@ -28,4 +28,7 @@ public sealed record CommandResult(bool Success, object? Payload = null, string?
     
     public static CommandResult ToggleHidden(string? message = null) => 
         new(true, Message: message, ShouldToggleHidden: true);
+    
+    public static CommandResult OpenFile(string pathToOpen, string? message = null) => 
+        new(true, Message: message, ShouldOpenFile: true, PathToOpen: pathToOpen);
 }
